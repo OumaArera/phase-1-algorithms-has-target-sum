86383a014923b06396cc8390226e1bd5906bfe4f
OumaArera <johnouma999@gmail.com>
2024-01-05 12:48:55 +0300

    feat: compare sum of two elements of array to target

--- index.js
+++ index.js
@@ -1,26 +1,35 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
   for (let i = 0; i < array.length; i++){
-    for (let j = i+1; j < array.length;){
+    for (let j = i+1; j < array.length; ){
       if (array[i] + array[j] === target) {
         return true;
       }
       j++;
+      // return false;S
     }
+    
   }
+  return false;
 }
 
 
 /* 
   Write the Big O time complexity of your function here
+  O(n*n) => O(n^2)
 */
 
 /* 
   Add your pseudocode here
+  Iterate throught the array at index i
+  Also iterate at index j
+  Sum elements of the array at index i and j 
+  Compare the sum to the target
 */
 
 /*
   Add written explanation of your solution here
+  Sum any two elements of the array and check if they are equal to target
 */
 
 // You can run `node index.js` to view these console logs
